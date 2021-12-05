0a790704498b79d083b422425a8d144a086d5014
Tiegris <tigristibi@gmail.com>
2021-12-05 12:37:22 +0100

    fox dal

--- canary_be/src/main/kotlin/hu/bme/mit/ftsrg/canary_be/dal/NameDay.kt
+++ canary_be/src/main/kotlin/hu/bme/mit/ftsrg/canary_be/dal/NameDay.kt
@@ -3,11 +3,12 @@ package hu.bme.mit.ftsrg.canary_be.dal
 import java.time.LocalDate
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 
 @Entity
 class NameDay(
-    @Id @GeneratedValue val id: Int?,
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Int?,
     val date: LocalDate,
     val name: String,
 )
